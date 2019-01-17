bc7afa5387da2931f8ee73027652e1b971b76e67
Anil Srivastava <anilsri_04@hotmail.com>
2019-01-17 18:29:37 +0530

    Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,12 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+class program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        //Create an empty List of type object
+        List<object> myList = new List<object>();
+
+        //Add 101, -90, "Hello", "World", "Git Community"
+        myList.Add(101);
+        myList.Add(-90);
+        myList.Add("Hello");
+        myList.Add("World");
+        myList.Add("Git Community");
+        myList.Add("false");
+        //Loop through the list, display all values and add all those that are type interface int
+        int sum = 0;
+        foreach (var obj in myList)
         {
+            Console.WriteLine(obj);
+            if (obj is int)
+            {
+                sum += (int)obj;
+            }
         }
+        Console.WriteLine("The sum of all the integers in the list is {0}", sum);
+        Console.ReadLine();
     }
+
 }
